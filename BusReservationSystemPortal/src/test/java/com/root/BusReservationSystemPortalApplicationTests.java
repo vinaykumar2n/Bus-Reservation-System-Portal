7367a6233463b2c82b0016a85f0a9b2621208fbf
Vinay Kumar <101393405+vinaykumar2n@users.noreply.github.com>
2022-09-27 15:32:50 +0530

    initial commit

---
+++ BusReservationSystemPortal/src/test/java/com/root/BusReservationSystemPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusReservationSystemPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
