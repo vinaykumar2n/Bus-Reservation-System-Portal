e7045b3fb731b91be886871b8fd07f602425bb8c
Vinay Kumar <naravinaykumar2@gmail.com>
2022-09-30 00:12:19 +0530

    updated feedback details

--- BusReservationSystemPortal/src/main/java/com/root/models/Feedback.java
+++ BusReservationSystemPortal/src/main/java/com/root/models/Feedback.java
@@ -8,6 +8,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Feedback {
 	
@@ -25,79 +28,6 @@ public class Feedback {
 	
 	@OneToOne
 	private Bus bus;
-	
-	public Feedback() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	
-	
-	public Feedback(Integer feedBackId, Integer driverRating, Integer serviceRating, Integer overallRating,
-			String comments, LocalDate feedbackdate, User user, Bus bus) {
-		super();
-		this.feedBackId = feedBackId;
-		this.driverRating = driverRating;
-		this.serviceRating = serviceRating;
-		this.overallRating = overallRating;
-		this.comments = comments;
-		this.feedbackdate = feedbackdate;
-		this.user = user;
-		this.bus = bus;
-	}
-
-
-
-
-
-	public Integer getFeedBackId() {
-		return feedBackId;
-	}
-	public void setFeedBackId(Integer feedBackId) {
-		this.feedBackId = feedBackId;
-	}
-	public Integer getDriverRating() {
-		return driverRating;
-	}
-	public void setDriverRating(Integer driverRating) {
-		this.driverRating = driverRating;
-	}
-	public Integer getServiceRating() {
-		return serviceRating;
-	}
-	public void setServiceRating(Integer serviceRating) {
-		this.serviceRating = serviceRating;
-	}
-	public Integer getOverallRating() {
-		return overallRating;
-	}
-	public void setOverallRating(Integer overallRating) {
-		this.overallRating = overallRating;
-	}
-	public String getComments() {
-		return comments;
-	}
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
-	public LocalDate getFeedbackdate() {
-		return feedbackdate;
-	}
-	public void setFeedbackdate(LocalDate feedbackdate) {
-		this.feedbackdate = feedbackdate;
-	}
-	public User getUser() {
-		return user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
-	public Bus getBus() {
-		return bus;
-	}
-	public void setBus(Bus bus) {
-		this.bus = bus;
-	}
-	
 	
 	
 	
