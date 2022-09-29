79f87b1ae15b99932be0b9dd31f8b6e8076e31a1
Vinay Kumar <naravinaykumar2@gmail.com>
2022-09-30 00:13:34 +0530

    updated reservation details

--- BusReservationSystemPortal/src/main/java/com/root/models/Reservation.java
+++ BusReservationSystemPortal/src/main/java/com/root/models/Reservation.java
@@ -12,131 +12,27 @@ import javax.persistence.OneToOne;
 
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reservation {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer reservationId;
 	private String reservationStatus;
-	private String reservationType;
 	private LocalDate reservationDate;
 	private LocalTime reservationTime;
 	private String source;
 	private String destination;
 	private Integer noOfSeatsBooked;
+	private Integer fare;
 	
 	@ManyToOne
 	private Bus bus;
 	
-	@OneToOne
+	@ManyToOne
 	private User user;
-	
-	public Reservation() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	
-
-	public Reservation(Integer reservationId, String reservationStatus, String reservationType,
-			LocalDate reservationDate, LocalTime reservationTime, String source, String destination,
-			Integer noOfSeatsBooked, Bus bus, User user) {
-		super();
-		this.reservationId = reservationId;
-		this.reservationStatus = reservationStatus;
-		this.reservationType = reservationType;
-		this.reservationDate = reservationDate;
-		this.reservationTime = reservationTime;
-		this.source = source;
-		this.destination = destination;
-		this.noOfSeatsBooked = noOfSeatsBooked;
-		this.bus = bus;
-		this.user = user;
-	}
-
-
-
-	public Integer getReservationId() {
-		return reservationId;
-	}
-
-	public void setReservationId(Integer reservationId) {
-		this.reservationId = reservationId;
-	}
-
-	public String getReservationStatus() {
-		return reservationStatus;
-	}
-
-	public void setReservationStatus(String reservationStatus) {
-		this.reservationStatus = reservationStatus;
-	}
-
-	public String getReservationType() {
-		return reservationType;
-	}
-
-	public void setReservationType(String reservationType) {
-		this.reservationType = reservationType;
-	}
-
-	public LocalDate getReservationDate() {
-		return reservationDate;
-	}
-
-	public void setReservationDate(LocalDate reservationDate) {
-		this.reservationDate = reservationDate;
-	}
-
-	public LocalTime getReservationTime() {
-		return reservationTime;
-	}
-
-	public void setReservationTime(LocalTime reservationTime) {
-		this.reservationTime = reservationTime;
-	}
-
-	public String getSource() {
-		return source;
-	}
-
-	public void setSource(String source) {
-		this.source = source;
-	}
-
-	public String getDestination() {
-		return destination;
-	}
-
-	public void setDestination(String destination) {
-		this.destination = destination;
-	}
-
-	public Integer getNoOfSeatsBooked() {
-		return noOfSeatsBooked;
-	}
-
-	public void setNoOfSeatsBooked(Integer noOfSeatsBooked) {
-		this.noOfSeatsBooked = noOfSeatsBooked;
-	}
-
-	public Bus getBus() {
-		return bus;
-	}
-
-	public void setBus(Bus bus) {
-		this.bus = bus;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-	
-	
-	
 	
 	
 }
