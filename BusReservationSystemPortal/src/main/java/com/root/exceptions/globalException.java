dad827ed6642c63a8fe481962780c4d7c967f8d1
ADARSH KHATRI <adkhatri250@gmail.com>
2022-09-30 19:21:51 +0530

    update file
    all done feedback work going on now

--- BusReservationSystemPortal/src/main/java/com/root/exceptions/globalException.java
+++ BusReservationSystemPortal/src/main/java/com/root/exceptions/globalException.java
@@ -4,25 +4,54 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
-public class globalException {
-
+public class GlobalException {
 	
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<myErrorDetail> exception(Exception e,WebRequest req) {
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Error> validatedException(MethodArgumentNotValidException validE,WebRequest web){
 		
-		myErrorDetail err = new myErrorDetail();
-		err.setTime(LocalDateTime.now());
-		err.setMsg(e.getMessage());
-		err.setDes(req.getDescription(false));
+		Error error = new Error(LocalDateTime.now(),validE.getMessage(),validE.getBindingResult().getFieldError().getDefaultMessage());
 		
-		return new ResponseEntity<myErrorDetail>(err,HttpStatus.BAD_REQUEST);
-		
+		return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
 	}
 	
+	@ExceptionHandler(BusException.class)
+	public ResponseEntity<Error> busException(BusException buse,WebRequest web){
+		
+		Error error = new Error(LocalDateTime.now(),buse.getMessage(), web.getDescription(false));
+		
+		return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(RouteException.class)
+	public ResponseEntity<Error> routeException(RouteException routeE,WebRequest web){
+		
+		Error error = new Error(LocalDateTime.now(),routeE.getMessage(), web.getDescription(false));
+		
+		return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<Error> noHandler(NoHandlerFoundException nohandler,WebRequest web){
+		
+		Error error = new Error(LocalDateTime.now(),nohandler.getMessage(), web.getDescription(false));
+		
+		return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+	}
+	
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Error> exception(Exception e,WebRequest web){ 
+		
+		Error error = new Error(LocalDateTime.now(),e.getMessage(), web.getDescription(false));
+		
+		return new ResponseEntity<>(error,HttpStatus.EXPECTATION_FAILED);
+	}
+		
 }
